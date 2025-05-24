2b7e7795b4a5b0965bcfc3a0b27742eac0efd9ba
lucasespina <lucaseqc@al.insper.edu.br>
2025-05-24 20:38:46 -0300

    teste novo

--- freertos/FreeRTOSConfig.h
+++ freertos/FreeRTOSConfig.h
@@ -103,11 +103,11 @@
 
 #if FREE_RTOS_KERNEL_SMP // set by the RP2040 SMP port of FreeRTOS
 /* SMP port only */
-#define configNUMBER_OF_CORES 1
+#define configNUMBER_OF_CORES 2
 #define configUSE_PASSIVE_IDLE_HOOK 0
 #define configTICK_CORE 0
 #define configRUN_MULTIPLE_PRIORITIES 1
-#define configUSE_CORE_AFFINITY 0
+#define configUSE_CORE_AFFINITY 1
 #endif
 
 /* RP2040 specific */
